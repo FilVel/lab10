79b11d3530b14e7941ccf134f2aace40196413db
Filippo Velli <filippo.velli@studio.unibo.it>
2023-12-05 11:41:13 +0100

    Done first part of exercise 10.2; tried a version without using builder pattern, it will be implemented later

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
@@ -1,8 +1,13 @@
 package it.unibo.mvc;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  */
@@ -18,7 +23,7 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
      * @param views
      *            the views to attach
      */
-    public DrawNumberApp(final DrawNumberView... views) {
+    public DrawNumberApp(final String configurationFile, final DrawNumberView... views) {
         /*
          * Side-effect proof
          */
@@ -27,7 +32,39 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
             view.setObserver(this);
             view.start();
         }
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+        Map<String, Integer> config = new HashMap<String, Integer>();
+        try (var configurations = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(configurationFile)))) {
+            for (var configuration = configurations.readLine(); configuration != null; configuration = configurations.readLine()) {
+                final String[] lineElements = configuration.split(":");
+                if (lineElements.length == 2) {
+                    final int value = Integer.parseInt(lineElements[1].trim());
+                    if (lineElements[0].contains("max")) {
+                        config.put("max", value);
+                    } else if (lineElements[0].contains("min")) {
+                        config.put("min", value);
+                    } else if (lineElements[0].contains("attempts")) {
+                        config.put("attemps", value);
+                    }
+                } else {
+                    for (final DrawNumberView view : this.views) {
+                        view.displayError("I can not understand \"" + configuration + '"');                        
+                    }
+                }
+            }
+        } catch (IOException e) {
+            for (final DrawNumberView view : this.views) {
+                view.displayError(e.getMessage());                        
+            }
+        }
+        if ((config.get("max") <= config.get("min")) || (config.get("attempts") <= 0)){
+            for (final DrawNumberView view : this.views) {
+                view.displayError("Incompatible configuration, using deafult one this time");
+                System.out.println(config.get("max"));                 
+            }
+            this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+        } else {
+            this.model = new DrawNumberImpl(config.get("min"), config.get("max"), config.get("attempts"));
+        }
     }
 
     @Override
@@ -66,7 +103,7 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
      * @throws FileNotFoundException 
      */
     public static void main(final String... args) throws FileNotFoundException {
-        new DrawNumberApp(new DrawNumberViewImpl());
+        new DrawNumberApp("src/main/resources/config.yml", new DrawNumberViewImpl());
     }
 
 }
--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberView.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberView.java
@@ -24,4 +24,9 @@ public interface DrawNumberView {
      * @param res the result of the last draw
      */
     void result(DrawResult res);
+
+    /**
+     * @param message error message that has to be shown to the user
+     */
+    void displayError(String message);
 }
